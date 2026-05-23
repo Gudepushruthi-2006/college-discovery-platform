b5396863ee99b65abf518754846d891dbf04ef3b
Shruthi <shruthiraju0406@gmail.com>
2026-05-23 12:55:41 +0530

    Initial commit

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,33 @@
+import prisma from "../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await prisma.college.createMany({
+    data: [
+      {
+        name: "IIT Hyderabad",
+        location: "Hyderabad",
+        fees: 200000,
+        rating: 4.8,
+        overview: "Top engineering institute in India.",
+        placements: "Highest package 60 LPA",
+        image:
+        "https://images.pexels.com/photos/159740/library-la-trobe-study-students-159740.jpeg",
+      },
+      {
+        name: "NIT Warangal",
+        location: "Warangal",
+        fees: 150000,
+        rating: 4.5,
+        overview: "Premier National Institute of Technology.",
+        placements: "Highest package 45 LPA",
+        image:
+          "https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg",
+      },
+    ],
+  });
+
+  return NextResponse.json({
+    message: "Database Seeded Successfully",
+  });
+}
